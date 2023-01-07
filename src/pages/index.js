2875580b3f1b699bb939b9dfab073a94e0d7aa1e
kyechan99 <kyechan99@gmail.com>
2023-01-07 21:06:59 +0900

    유투브 주소 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -103,7 +103,7 @@ const IndexPage = () => {
             className="yt-iframe"
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/"
+            src="https://www.youtube.com/embed/bssHujvUd3o"
             title="YouTube video player"
             // frameBorder={"0"}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
